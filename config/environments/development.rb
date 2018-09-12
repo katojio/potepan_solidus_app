2cdd01021dc216cf8f4d4b7fb08e306ce04f04e0
motchang <motchang@gmail.com>
2018-09-12 21:36:01 +0900

    Fix Docker runtime config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # add white list ips under Docker
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
