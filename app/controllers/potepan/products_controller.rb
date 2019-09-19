42486b215e3d859319f70af2838318d4ea2788b5
加藤航大 <kodai@Kodais-MacBook-Air.local>
2019-09-19 22:24:43 +0900

    Resolve errors of rubocop

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -10,35 +10,38 @@ module Potepan
       @variant    = Spree::Variant.find_by(product_id: @product.id)
       @images     = Spree::Image.where(viewable_id: @variant.id)
 
-      # 関連画像を探す処理
-      # 配列を準備
+      # Search related images
+      # Prepare empty array
       taxon_ids = []
       product_ids = []
 
-      # 表示している商品のidから、その商品の分類(Taxon)idを取得する
+      # Determine taxon_id from id of showing product
       classifications = Spree::Classification.where(product_id: params[:id])
       classifications.each do |c|
         taxon_ids << c.taxon_id
       end
 
-      # Spree::Classificationモデルから、先ほど取得した1つ目のtaxon_idを持ち、かつproduct_idが表示している商品でない商品のidを取得する
-      related_classifications = Spree::Classification.where(taxon_id: taxon_ids[0]).where.not(product_id: params[:id])
+      # Determine product_id which has first taxon_id and not has product_id of
+      #  showing product from Spree::Classification
+      related_classifications = Spree::Classification.where(taxon_id:
+        taxon_ids[0]).where.not(product_id: params[:id])
       related_classifications.each do |c|
         product_ids.push(c.product_id)
       end
 
-      # 上と同じ処理を2つ目のtaxon_idを用いて行う
-      related_classifications = Spree::Classification.where(taxon_id: taxon_ids[1]).where.not(product_id: params[:id])
+      # Same process as before by second taxon_id
+      related_classifications = Spree::Classification.where(taxon_id:
+        taxon_ids[1]).where.not(product_id: params[:id])
       related_classifications.each do |c|
         product_ids.push(c.product_id)
       end
 
-      # 配列を準備
+      # Prepare array
       @products = []
       variants = []
       @related_images = []
 
-      # これまでの処理で集めたproduct_idの集合を元に、関連商品の画像を取得する
+      # Get related images by product_ids
       product_ids.each do |p|
         @products << Spree::Product.find(p)
         variants  << Spree::Variant.find_by(product_id: p)
@@ -47,12 +50,14 @@ module Potepan
         @related_images << Spree::Image.find_by(viewable_id: v.id)
       end
 
-      # JavaScriptへの対応
+      # JavaScript
       respond_to do |format|
-      format.html { render 'single_product' }
-      format.js
+        format.html { render 'single_product' }
+        format.js
       end
     end
+
+    # rubocop:disable Style/RedundantReturn
 
     # GET/potepan/products/:id/edit_l_pic
     # params[:id], params[:variant_id]
@@ -60,8 +65,9 @@ module Potepan
       @product  ||= Spree::Product.find(params[:id])
       @variant    = Spree::Variant.find(params[:variant_id])
       @images     = Spree::Image.where(viewable_id: @variant.id)
-      # 画像のレンダリング部分を返す
-      return (render partial: "image", collection: @images, locals: {picture_size: "large"})
+      # return the part of image rendering
+      return (render partial: 'image', collection: @images,
+                     locals: { picture_size: 'large' })
     end
 
     # GET/potepan/products/:id/edit_s_pic
@@ -70,9 +76,10 @@ module Potepan
       @product  ||= Spree::Product.find(params[:id])
       @variant    = Spree::Variant.find(params[:variant_id])
       @images     = Spree::Image.where(viewable_id: @variant.id)
-      # 画像のレンダリング部分を返す
-      return (render partial: "image", collection: @images, locals: {picture_size: "small"})
+      # return the part of image rendering
+      return (render partial: 'image', collection: @images,
+                     locals: { picture_size: 'small' })
     end
-
+    # rubocop:enable Style/RedundantReturn
   end
 end
