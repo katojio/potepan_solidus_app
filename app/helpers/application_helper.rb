9308df9a77de4bc2824e7cb1f3a1f3e077af2de0
加藤航大 <kodai@Kodais-MacBook-Air.local>
2019-09-19 20:41:53 +0900

    Add partial of header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # application_helper
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title="")
+    base_title = "BIGBAG Store"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
+  
 end
