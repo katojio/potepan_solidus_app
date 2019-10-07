6a4096a6290fbbdde92b1cdf5101dfbfebe3ae28
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-07 11:43:16 +0900

    Add categories/show action and view

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,19 @@
 module Potepan
   class CategoriesController < ApplicationController
     def show
+      classifications = Spree::Classification.where(taxon_id: params[:id])
+      @products = []
+      classifications.each do |c|
+        @products << Spree::Product.find(c.product_id)
+      end
+      @variants = []
+      @products.each do |p|
+        @variants << Spree::Variant.find_by(product_id: p.id)
+      end
+      @images = []
+      @variants.each do |v|
+        @images << Spree::Image.find_by(viewable_id: v.id)
+      end
     end
   end
 end
