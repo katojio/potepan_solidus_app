a851d672c039a74c93b46ecc6630e4e7246aa6d2
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-07 18:01:04 +0900

    Implement categories tree dynamic

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -14,6 +14,21 @@ module Potepan
       @variants.each do |v|
         @images << Spree::Image.find_by(viewable_id: v.id)
       end
+
+      categories_taxonomy_id = Spree::Taxonomy.find_by(name: "Categories").id
+      category_taxons = Spree::Taxon.where(taxonomy_id:
+        categories_taxonomy_id).where.not(parent_id: nil)
+      @large_categories = []
+      @small_categories = []
+      @products_count = []
+      category_taxons.each do |t|
+        if t.parent_id == 1
+          @large_categories << t
+        else
+          @small_categories << t
+        end
+        @products_count[t.id] = Spree::Classification.where(taxon_id: t.id).count
+      end
     end
   end
 end
