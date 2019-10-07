074c9d3a5fcae30b03d638d7bf00c17648d95ab6
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-07 22:23:20 +0900

    Refactor products_controller and modify relating files

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,46 +3,21 @@ module Potepan
     def show
       @product  ||= Spree::Product.find(params[:id])
       @variants   = Spree::Variant.where(product_id: @product.id)
-      @variant    = @variants.first
-      @images     = Spree::Image.where(viewable_id: @variant.id)
+      @images     = @product.images
 
       # Search related images
-      taxon_ids = []
-      product_ids = []
-
-      classifications = Spree::Classification.where(product_id: params[:id])
-      classifications.each do |c|
-        taxon_ids << c.taxon_id
-      end
-
-      related_classifications = Spree::Classification.where(taxon_id:
-        taxon_ids[0]).where.not(product_id: params[:id])
-      related_classifications.each do |c|
-        product_ids.push(c.product_id)
-      end
-
-      related_classifications = Spree::Classification.where(taxon_id:
-        taxon_ids[1]).where.not(product_id: params[:id])
-      related_classifications.each do |c|
-        product_ids.push(c.product_id)
-      end
-
-      @products = []
-      variants = []
+      @related_products = []
       @related_images = []
-
-      product_ids.each do |p|
-        product = Spree::Product.find(p)
-        unless @products.include?(product)
-          @products << product
-          variants  << Spree::Variant.find_by(product_id: p)
+      taxons = Spree::Product.find(params[:id]).taxons
+      taxons.each do |t|
+        t.products.each do |p|
+          if ( (p.id != @product.id) && !(@related_products.include?(p)) )
+            @related_products << p
+          end
         end
       end
-      variants.each do |v|
-        image = Spree::Image.find_by(viewable_id: v.id)
-        unless @related_images.include?(image)
-          @related_images << image
-        end
+      @related_products.each do |r|
+        @related_images << r.images.first
       end
 
       respond_to do |format|
