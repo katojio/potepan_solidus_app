9c1f64702d2084f907ace20b06d8ba218ecca612
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-14 14:31:25 +0900

    Add categories_controller test of assigns(:products)

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Potepan::CategoriesController, type: :controller do
   describe "カテゴリーページ" do
     let!(:taxon) { create(:taxon) }
+    let!(:product) { create(:product, taxons: [taxon]) }
 
     before do
       get :show, params: { id: taxon.id }
@@ -11,13 +12,14 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
     it "レスポンスが正常である" do
       expect(response).to be_successful
     end
-
     it "カテゴリーページが表示される" do
       expect(response).to render_template :show
     end
-
     it "@taxonがアサインされる" do
       expect(assigns(:taxon)).to eq taxon
+    end
+    it "@productsがアサインされる" do
+      expect(assigns(:products)).to match_array(product)
     end
   end
 end
