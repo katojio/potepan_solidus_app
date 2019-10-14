9ee4e2698fd550a1e75bac7459a094472ca365b8
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-14 14:32:07 +0900

    Refactor categories_controller and relating views

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -4,21 +4,11 @@ module Potepan
       @taxon    = Spree::Taxon.find(params[:id])
       @products = @taxon.products
 
-      category_taxonomy = Spree::Taxonomy.find_by(name: "Categories")
-      categories_taxonomy_id = category_taxonomy.id unless category_taxonomy.nil?
+      category_taxonomy_id = Spree::Taxonomy.find_by(name: "Categories").id
       category_taxons = Spree::Taxon.where(taxonomy_id:
-        categories_taxonomy_id).where.not(parent_id: nil)
-      @large_categories = []
-      @small_categories = []
-      @products_count = []
-      category_taxons.each do |t|
-        if t.parent_id == 1
-          @large_categories << t
-        else
-          @small_categories << t
-        end
-        @products_count[t.id] = Spree::Taxon.find(t.id).products.count
-      end
+        category_taxonomy_id).where.not(parent_id: nil)
+      @large_categories = category_taxons.select { |t| t.parent_id == category_taxonomy_id }
+      @small_categories = category_taxons.select { |t| t.parent_id != category_taxonomy_id }
     end
   end
 end
