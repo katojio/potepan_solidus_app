3a610b5dfabe853df7bca7ab50d0e8331ac4f696
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-14 14:49:26 +0900

    Refactor categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -4,11 +4,10 @@ module Potepan
       @taxon    = Spree::Taxon.find(params[:id])
       @products = @taxon.products
 
-      category_taxonomy_id = Spree::Taxonomy.find_by(name: "Categories").id
-      category_taxons = Spree::Taxon.where(taxonomy_id:
-        category_taxonomy_id).where.not(parent_id: nil)
-      @large_categories = category_taxons.select { |t| t.parent_id == category_taxonomy_id }
-      @small_categories = category_taxons.select { |t| t.parent_id != category_taxonomy_id }
+      category_taxonomy = Spree::Taxonomy.find_by(name: "Categories")
+      category_taxons   = category_taxonomy.taxons
+      @large_categories = category_taxons.select { |t| t.parent_id == category_taxonomy.id }
+      @small_categories = category_taxons.select { |t| t.parent_id != category_taxonomy.id }
     end
   end
 end
