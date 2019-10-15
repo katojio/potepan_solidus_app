9e2b856c6bce1e11f1f3a84551e84b6fd19a5474
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-15 21:06:38 +0900

    Modify categories_controller to prevent the errors by using method for nil:NilClass

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -4,10 +4,13 @@ module Potepan
       @taxon    = Spree::Taxon.find(params[:id])
       @products = @taxon.products
 
-      category_taxons   = Spree::Taxonomy.find_by(name: 'Categories').taxons
+      category_taxonomy = Spree::Taxonomy.find_by(name: 'Categories')
+      category_taxons   = category_taxonomy.taxons unless category_taxonomy.nil?
       category_taxon    = category_taxons.find_by(name: 'Categories')
-      @large_categories = category_taxons.select { |t| t.parent_id == category_taxon.id }
-      @small_categories = category_taxons.select { |t| t.parent_id != category_taxon.id }
+      unless category_taxon.nil?
+        @large_categories = category_taxons.select { |t| t.parent_id == category_taxon.id }
+        @small_categories = category_taxons.select { |t| t.parent_id != category_taxon.id }
+      end
     end
   end
 end
