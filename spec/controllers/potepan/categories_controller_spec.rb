b5dc5c9d0ffa5fed41add1695391de10b92be710
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-16 21:57:41 +0900

    Insert empty row between it and it in RSpec test

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -14,19 +14,24 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
     it "レスポンスが正常である" do
       expect(response).to be_successful
     end
+
     it "カテゴリーページが表示される" do
       expect(response).to render_template :show
     end
+
     it "@taxonがアサインされる" do
       expect(assigns(:taxon)).to eq taxon
     end
+
     it "@productsがアサインされる" do
       expect(assigns(:products)).to match_array(product)
     end
+
     it "@large_categoriesがアサインされる" do
       large_categories = category_taxons.select { |t| t.parent_id == taxon.id }
       expect(assigns(:large_categories)).to eq large_categories
     end
+
     it "@small_categoriesがアサインされる" do
       small_categories = category_taxons.select { |t| t.parent_id != taxon.id }
       expect(assigns(:small_categories)).to eq small_categories
--- spec/system/potepan/categories_spec.rb
+++ spec/system/potepan/categories_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe "Potepan::Categories", type: :system do
       it "表示タイトルが正しいことを確認" do
         expect(page).to have_title 'Category - BIGBAG Store'
       end
+
       it "カテゴリー名がページ上部に表示されることを確認" do
         expect(page).to have_css('h2', text: "#{taxon_b.name}")
       end
@@ -29,16 +30,19 @@ RSpec.describe "Potepan::Categories", type: :system do
       it "カテゴリー名がカテゴリーツリーに表示されることを確認" do
         expect(page).to have_css('li', text: "#{taxon_b.name}")
       end
+
       it "表示されるカテゴリー(liタグ)が合計7つであることを確認" do
         within find('#category') do
           expect(all('li').size).to eq(7)
         end
       end
+
       it "表示される大カテゴリーが合計3つであることを確認" do
         within find('#category') do
           expect(page).to have_selector '.collapseItem', count: 3
         end
       end
+
       it "カテゴリー詳細ページへのリンクが貼られていることを確認" do
         within find("#category#{taxon_c.id}") do
           expect(page).to have_link taxon_c.name, href: potepan_category_path(taxon_c.id)
@@ -50,12 +54,15 @@ RSpec.describe "Potepan::Categories", type: :system do
       it "カテゴリーに該当する商品の名前があることを確認" do
         expect(page).to have_content product.name
       end
+
       it "カテゴリーに該当する商品の価格があることを確認" do
         expect(page).to have_content product.display_price
       end
+
       it "カテゴリーに該当する商品の詳細ページへのリンクがあることを確認" do
         expect(page).to have_link(href: potepan_product_path(product.id))
       end
+      
       it "カテゴリーに該当しない商品の名前がないことを確認" do
         expect(page).to have_no_content other_product.name
       end
