8ad7371666a2f7ca12d8026423bd9665ccd6f2c6
Kodai Kato <greatvoyage0122@gmail.com>
2019-11-08 16:49:16 +0900

    Modify production.rb again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -23,7 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
